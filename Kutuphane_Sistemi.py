3a4dbbdf0b39ed0989d053b44050aabbf93c9bef
oznur123456 <oznur.demirkaya@gmail.com>
2025-08-23 20:20:00 +0300

    proje2

--- Kutuphane_Sistemi.py
+++ Kutuphane_Sistemi.py
@@ -1,25 +1,27 @@
 def selamla():
-    print("Merhaba!")
+    print("Merhaba! Kutuphane sistemine hos geldiniz.")
     
+def kutuphane_anamenu():
+    print("\033[1;35;40m")
+    print("╔════════════════════════════════╗")
+    print("║\033[1;33;40m        KUTUPHANE SISTEMI     \033[1;35;40m  ║")
+    print("║                                ║")
+    print("║  1-Kitap Ekle                  ║")
+    print("║  2-Kitap Ara                   ║")
+    print("║  3-Guncel Kitap Listesi        ║")
+    print("║  4-Odunc Alma Islemi           ║")
+    print("║  5-Ogrenci Kitap Okuma Verisi  ║")
+    print("║  6-Cikis                       ║")
+    print("║                                ║")
+    print("║   Seçiminiz nedir?             ║")
+    print("╚════════════════════════════════╝")
+    print("\033[0m") 
+    
+selamla()
+
 while True: 
-    def kutuphane_anamenu():
-        print("\033[1;35;40m")
-        print("╔═════════════════════════════════╗")
-        print("║\033[1;33;40m     KUTUPHANE SISTEMI    \033[1;35;40m  ║")
-        print("║                                 ║")
-        print("║  1-Kitap Ekle                   ║")
-        print("║  2-Kitap Ara                    ║")
-        print("║  3-Tum Kitaplari Listele        ║")
-        print("║  4-Odunc Alma Islemi            ║")
-        print("║  5-Ogrenci Kitap Okuma Verisi   ║")
-        print("║  6-Cikis                        ║")
-        print("║                                 ║")
-        print("║   Seçiminiz nedir?              ║")
-        print("╚═════════════════════════════════╝")
-        print("\033[0m") 
-
+    kutuphane_anamenu()
     secim = (input("Seçiminiz icin bir sayi giriniz (1-6): "))
-
 
     if not secim.isdigit():
         print("Lütfen bir sayi giriniz.")
@@ -32,9 +34,9 @@ while True:
         continue
 
     if secim == 1: 
-        print ("Lutfen eklemek istediginiz kitap bilgilerini girin.\n")
-        import Kitap_Listesi 
-        Kitap_Listesi.kitap_ekle()
+        print ("Lutfen eklemek istediginiz kitap bilgilerini girin.")
+        import Kitap_Listesi_Olusturma 
+        Kitap_Listesi_Olusturma.kitap_ekle()
 
     elif secim == 4: 
         break 
