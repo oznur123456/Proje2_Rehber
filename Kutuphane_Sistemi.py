06607b0a788ec09e507603d2e73c1a7bcf10153a
oznur123456 <oznur.demirkaya@gmail.com>
2025-08-23 20:05:57 +0300

    proje2

--- Kutuphane_Sistemi.py
+++ Kutuphane_Sistemi.py
@@ -1,11 +1,8 @@
 def selamla():
     print("Merhaba!")
     
-
-
 while True: 
     def kutuphane_anamenu():
-
         print("\033[1;35;40m")
         print("╔═════════════════════════════════╗")
         print("║\033[1;33;40m     KUTUPHANE SISTEMI    \033[1;35;40m  ║")
@@ -21,32 +18,27 @@ while True:
         print("╚═════════════════════════════════╝")
         print("\033[0m") 
 
-    secim = int(input("Seçiminiz icin bir sayi giriniz (1-6): "))
+    secim = (input("Seçiminiz icin bir sayi giriniz (1-6): "))
 
 
-    if secim.isalpha or secim < 1 or secim > 6:
+    if not secim.isdigit():
+        print("Lütfen bir sayi giriniz.")
+        continue
+        
+    secim = int(secim)
+
+    if secim < 1 or secim > 6:
         print("Secim yanlis. 1-6 arasi bir deger giriniz.")
         continue
 
-    if secim == "1": 
+    if secim == 1: 
         print ("Lutfen eklemek istediginiz kitap bilgilerini girin.\n")
         import Kitap_Listesi 
         Kitap_Listesi.kitap_ekle()
-        
 
-    elif secim == "2": 
-        aranan = input("Aranan kitap adı: ")
-        aranan =
-
-
-        if aranan in Kitap_listesi.txt:
-            print (f"{aranan} kutuphanede mevcut.")
-        else:
-            print("Yazar:", kutuphane.get(aranan, "Bu kitap bulunamadı.\n"))
-        print("Kitabin") 
-    elif secim == "3": 
-        for kitap, yazar in kutuphane.items(): 
-        print(f"{kitap} - {yazar}") 
-    elif secim == "4": 
+    elif secim == 4: 
         break 
-    else: print("Geçersiz seçim.")
+
+
+selamla()
+kutuphane_anamenu()
