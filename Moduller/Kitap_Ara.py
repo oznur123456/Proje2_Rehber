650c233db01d2b713277b1335cf96a554ac78544
oznur123456 <oznur.demirkaya@gmail.com>
2025-09-05 16:10:59 +0300

    proje2

--- Moduller/Kitap_Ara.py
+++ Moduller/Kitap_Ara.py
@@ -1,15 +1,15 @@
 print("\n KITAP ARAMA MENUSU \n")
 
-import Guncel_Kitap_Listesi 
-Guncel_Kitap_Listesi.kitaplari_goster()
-
 def aranan_kitap():
-    aranan = input("Aranan kitap adi: ")
+    aranan = input("Aranan kitap adi: ").strip()
 
-    with open("Kitap_listesi.txt", "r", encoding="utf-8") as f:
-        kitaplar = f.read().splitlines()
+    with open("Kitap_Listesi_Olusturma.txt", "r", encoding="utf-8") as dosya:
+        for satir in dosya:
+            if satir.startswith("Kitap: "):
+                kitap_adi = satir.split(",")[0].replace("Kitap: ", "").strip()
+                if aranan.lower() == kitap_adi.lower():
+                    print(f"{aranan} adli kitap kutuphanede mevcut.")
+                    return
+    print("Aradiginiz kitap kutuphanede mevcut degil.")
 
-    if aranan in kitaplar:
-        print(f"{aranan} kütüphanede mevcut.")
-    else:
-        print("Aradiginiz kitap kutuphanede mevcut degil.")
+aranan_kitap()
