998978a63c3087be328cfef0aaf3102429d9b2b5
oznur123456 <oznur.demirkaya@gmail.com>
2025-09-05 16:16:16 +0300

    proje2

--- Moduller/Kitap_Talep_Listesi.py
+++ Moduller/Kitap_Talep_Listesi.py
@@ -4,8 +4,8 @@ def kitap_talep():
     isim = input("Kitap adi: ")
     yazar = input("Yazar adi: ")
     yili = input("Kitabin yayin tarihi: ")
-    with open("Kitap_Listesi_Olusturma.txt", "a", encoding="utf-8") as dosya: 
+    with open("Kitap_Talep_Listesi.txt", "a", encoding="utf-8") as dosya: 
         dosya.write(f"Kitap: {isim}\t\t\t Yazar: {yazar}\t\t\t Yayin tarihi: {yili}\n")
-    print("\nKitap ekleme islemi tamamlandi.")
+    print("\nKitap talebiniz alinmistir.")
 
-kitap_ekle()
+kitap_talep()
