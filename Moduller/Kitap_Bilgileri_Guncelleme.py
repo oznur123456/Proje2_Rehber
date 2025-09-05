cdb888f0d1dca4a1b09571d2201de406fa8fe5ef
oznur123456 <oznur.demirkaya@gmail.com>
2025-09-05 16:01:00 +0300

    proje2

--- Moduller/Kitap_Bilgileri_Guncelleme.py
+++ Moduller/Kitap_Bilgileri_Guncelleme.py
@@ -1,23 +1,28 @@
-print("KITAP KAYDI SILME\n")
+print("KITAP BILGILERI GUNCELLEME\n")
 
-def kitap_sil():
-
+def kitap_degistir():
     with open("Kitap_Listesi_Olusturma.txt", "r", encoding="utf-8") as dosya:
         satir_list = dosya.readlines()
 
     print("Güncel Kitap Listesi:\n")
-
     for i, satir in enumerate(satir_list, start=1):
         print(f"{i}. {satir.strip()}")
 
-    sil_numara = int(input("\nSilmek istediginiz kitabin numarasini giriniz: "))
+    degistir_numara = int(input("\nBilgilerini degistirmek istediginiz kitabin numarasini giriniz: "))
+
+    # Kullanıcıdan yeni bilgiler
+    yeni_isim = input("Yeni kitap adi: ")
+    yeni_yazar = input("Yeni yazar adi: ")
+    yeni_yil = input("Yeni yayin tarihi: ")
 
     with open("Kitap_Listesi_Olusturma.txt", "w", encoding="utf-8") as dosya:
         for i, satir in enumerate(satir_list, start=1):
-            if i != sil_numara:
+            if i == degistir_numara:
+                dosya.write(f"Kitap: {yeni_isim},\t\t Yazar: {yeni_yazar},\t\t Yayin tarihi: {yeni_yil}\n")
+            else:
                 dosya.write(satir)
 
-    print("\nKitap kaydi basariyla silindi.\n")
+    print("\nKitap kaydi basariyla guncellendi.\n")
 
     with open("Kitap_Listesi_Olusturma.txt", "r", encoding="utf-8") as dosya:
         satir_list = dosya.readlines()
@@ -26,5 +31,4 @@ def kitap_sil():
     for i, satir in enumerate(satir_list, start=1):
         print(f"{i}. {satir.strip()}")
 
-kitap_sil()
-
+kitap_degistir()
